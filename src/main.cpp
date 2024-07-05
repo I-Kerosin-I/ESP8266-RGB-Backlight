e5e124f8a9f41337907f23872c77cd0b31b738a7
I-Kerosin-I <max.andr2005@yandex.ru>
2024-07-05 15:48:48 +0300

    added mode control by encoder strings moved to flash

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 
 #include "settings.h"
 #include "udpUtils.h"
+#include "color.h"
 #include "test.h"
 
 
@@ -90,13 +91,13 @@ void setup()
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
-    DBG_PRINT(".");
+    DBG_PRINT(F("."));
   }
 
-  DBG_PRINTLN("");
-  DBG_PRINTLN("WiFi connected");
+  DBG_PRINTLN(F(""));
+  DBG_PRINTLN(F("WiFi connected"));
   udpClient.begin(UDP_PORT);
-  DBG_PRINTLN("Server started");
+  DBG_PRINTLN(F("Server started"));
   irrecv.enableIRIn();
   pinMode(S1_PIN, INPUT);
   pinMode(S2_PIN, INPUT);
@@ -118,7 +119,7 @@ void loop()
 
     if (millis() - reconnect_timer >= RECONNECT_DELAY)// Переподключение к WI-FI
     {
-      DBG_PRINTLN("Reconnecting to WIFI network");
+      DBG_PRINTLN(F("Reconnecting to WIFI network"));
       if(WiFi.reconnect()) 
       {
         indicatorState = false;
@@ -156,11 +157,11 @@ void loop()
         udpSend(dataToSend, modeDataArrayLengths[dataToSend[0]] + 3, remoteIP);
         clientIps[client_amount++] = remoteIP;
         KeepAliveIpBuffer[KeepAliveIpBufferIndex++] = remoteIP;
-        DBG_PRINTLN("Запрос на создание канала связи");
+        DBG_PRINTLN(F("Запрос на создание канала связи"));
       }
       else
       {
-        DBG_PRINTLN("Попытка дублировать соединение");
+        DBG_PRINTLN(F("Попытка дублировать соединение"));
       }
     }
 
@@ -170,7 +171,7 @@ void loop()
       updateDataToSend();
       for (int i = 0; i < client_amount; i++)
         udpSend(dataToSend, modeDataArrayLengths[dataToSend[0]] + HEADER_LENGTH, clientIps[i]);
-      DBG_PRINTLN("Запрос на смену режима");
+      DBG_PRINTLN(F("Запрос на смену режима"));
     }
 
     else if(packetBuffer[1] & 0b01000000)
@@ -178,20 +179,20 @@ void loop()
       updateDataToSend();
       for (int i = 0; i < client_amount; i++) if(clientIps[i] != remoteIP) udpSend(dataToSend, modeDataArrayLengths[dataToSend[0]] + HEADER_LENGTH, clientIps[i]);
       diode.setBrightness(packetBuffer[2]);
-      DBG_PRINTLN("Запрос на измененние яркости");
+      DBG_PRINTLN(F("Запрос на измененние яркости"));
     }
 
     else if (packetBuffer[1] & 0b00000010) // Запрос на измененние состояния вкл/выкл
     {
       changeEnabledState(packetBuffer[1] & 0b00000001, remoteIP);
-      DBG_PRINTLN("Запрос на измененние состояния вкл/выкл");
+      DBG_PRINTLN(F("Запрос на измененние состояния вкл/выкл"));
     }
 
     else if (packetBuffer[1] & 0b00001000) // Запрос текущего состояния
     {
       updateDataToSend();
       udpSend(dataToSend, modeDataArrayLengths[dataToSend[0]] + HEADER_LENGTH, remoteIP);
-      DBG_PRINTLN("Запрос текущего состояния");
+      DBG_PRINTLN(F("Запрос текущего состояния"));
     }
 
     else if ((packetBuffer[0] != curMode) || ((packetBuffer[1] & 1) != isEnabled)) // Не совпадает режим или сост. вкл/выкл
@@ -230,28 +231,76 @@ void loop()
 
   eb.tick();
 
-  if (eb.hold()) changeEnabledState(!isEnabled); // TODO: Добавить рассылку-синхронизацию
+  if (eb.hold() && !eb.getClicks()) changeEnabledState(!isEnabled); // TODO: Добавить рассылку-синхронизацию
   
   if (eb.turn())
   {                                                   // Поворот энкодера
     /*
-        DBG_PRINT("turn: dir ");
+        DBG_PRINT(F("turn: dir "));
         DBG_PRINT(eb.dir());
-        DBG_PRINT(", fast ");
+        DBG_PRINT(F(", fast "));
         DBG_PRINT(eb.fast());
-        DBG_PRINT(", hold ");
+        DBG_PRINT(F(", hold "));
         DBG_PRINT(eb.pressing());
-        DBG_PRINT(", counter ");
+        DBG_PRINT(F(", counter "));
         DBG_PRINT(eb.counter);
-        DBG_PRINT(", clicks ");
+        DBG_PRINT(F(", clicks "));
         DBG_PRINTLN(eb.getClicks());
     */
     if (eb.pressing())
     {
-      // if (eb.getClicks())
-      // {
-      //   modeDataArrayPtrs[curMode][constrain(eb.getClicks() - 1, 0, modeDataArrayLengths[curMode] - 1)] += eb.dir() * 10;
-      // } else
+      if (eb.getClicks())
+      {
+        // modeDataArrayPtrs[curMode][constrain(eb.getClicks() - 1, 0, modeDataArrayLengths[curMode] - 1)] += eb.dir() * 10;
+        switch (curMode)
+        {
+        case 0:    // RGB
+          switch (eb.getClicks())
+          {
+          case 1:
+            shiftByHsv(rgbData, eb.dir() * 5, 0);
+            DBG_PRINT(F("RGB SHIFTED BY ENCODER: "));
+            DBG_PRINT(rgbData[0]);
+            DBG_PRINT(F(" "));
+            DBG_PRINT(rgbData[1]);
+            DBG_PRINT(F(" "));
+            DBG_PRINTLN(rgbData[2]);
+            break;
+          case 2:
+            shiftByHsv(rgbData, 0, eb.dir() * 5);
+            DBG_PRINT(F("RGB SHIFTED BY ENCODER: "));
+            DBG_PRINT(rgbData[0]);
+            DBG_PRINT(F(" "));
+            DBG_PRINT(rgbData[1]);
+            DBG_PRINT(F(" "));
+            DBG_PRINTLN(rgbData[2]);
+            break;
+          }
+          break;
+        case 1:    // Rainbow
+          switch (eb.getClicks())
+          {
+          case 1:
+            rainbowData[0] = constrain(rainbowData[0]+eb.dir(), 0, 50);
+            break;
+          case 2:
+            rainbowData[1] = constrain(rainbowData[1]+eb.dir(), 0, 255);
+            break;
+          }
+          break;
+        case 2:    // Fire
+          switch (eb.getClicks())
+          {
+          case 1:
+            fireData[0] = constrain(fireData[0] + eb.dir() * 5, 0, 255);
+            break;
+          case 2:
+            fireData[1] = constrain(fireData[1] + eb.dir() * 5, 0, 255);
+            break;
+          }
+          break;
+        }
+      } else
         curMode = (curMode + eb.dir() + MODE_AMOUNT) % MODE_AMOUNT; // TODO: Добавить рассылку-синхронизацию
     } else {
       diode.setBrightness(constrain(diode.getBrightness() + 10 * eb.dir(), 0, 255));
@@ -266,29 +315,29 @@ void loop()
     // #if DEBUG_EN
     // serialPrintUint64(IR_results.value, HEX); 
     // #endif
-    // DBG_PRINTLN("");
+    // DBG_PRINTLN(F(""));
 
     switch (IR_results.value)
     {
       case 0xF700FF:        // bri ↑
         diode.setBrightness(constrain(diode.getBrightness() + 10, 0, 255));
-        DBG_PRINTLN("IR: BRI UP");
+        DBG_PRINTLN(F("IR: BRI UP"));
         break;
 
       case 0xF7807F:        // bri ↓
         diode.setBrightness(constrain(diode.getBrightness() - 10, 0, 255));
-        DBG_PRINTLN("IR: BRI DOWN");
+        DBG_PRINTLN(F("IR: BRI DOWN"));
         break;
 
       case 0xF740BF:        // ВЫКЛ
         isEnabled = false;
         diode.setRGB(0, 0, 0);
-        DBG_PRINTLN("IR: ВЫКЛ");
+        DBG_PRINTLN(F("IR: ВЫКЛ"));
         break;
 
       case 0xF7C03F:        // ВКЛ
         isEnabled = true;
-        DBG_PRINTLN("IR: ВКЛ");
+        DBG_PRINTLN(F("IR: ВКЛ"));
         break;
 
       case 0xF720DF:        // RED
@@ -296,7 +345,7 @@ void loop()
         rgbData[0] = 255;
         rgbData[1] = 0;
         rgbData[2] = 0;
-        DBG_PRINTLN("IR: RED");
+        DBG_PRINTLN(F("IR: RED"));
         break;
 
       case 0xF7A05F:        // GREEN
@@ -304,7 +353,7 @@ void loop()
         rgbData[0] = 0;
         rgbData[1] = 255;
         rgbData[2] = 0;
-        DBG_PRINTLN("IR: GREEN");
+        DBG_PRINTLN(F("IR: GREEN"));
         break;
 
       case 0xF7609F:        // BLUE
@@ -312,7 +361,7 @@ void loop()
         rgbData[0] = 0;
         rgbData[1] = 0;
         rgbData[2] = 255;
-        DBG_PRINTLN("IR: BLUE");
+        DBG_PRINTLN(F("IR: BLUE"));
         break;
 
       case 0xF7E01F:        // WHITE
@@ -320,7 +369,7 @@ void loop()
         rgbData[0] = 255;
         rgbData[1] = 255;
         rgbData[2] = 255;
-        DBG_PRINTLN("IR: WHITE");
+        DBG_PRINTLN(F("IR: WHITE"));
         break;
       
       case 0xF710EF:        // COL 1
@@ -328,7 +377,7 @@ void loop()
         rgbData[0] = 255;
         rgbData[1] = 24;
         rgbData[2] = 0;
-        DBG_PRINTLN("IR: COL 1");
+        DBG_PRINTLN(F("IR: COL 1"));
         break;
 
       case 0xF7906F:        // COL 2
@@ -336,7 +385,7 @@ void loop()
         rgbData[0] = 0;
         rgbData[1] = 255;
         rgbData[2] = 43;
-        DBG_PRINTLN("IR: COL 2");
+        DBG_PRINTLN(F("IR: COL 2"));
         break;
 
       case 0xF750AF:        // COL 3
@@ -344,12 +393,12 @@ void loop()
         rgbData[0] = 0;
         rgbData[1] = 38;
         rgbData[2] = 255;
-        DBG_PRINTLN("IR: COL 3");
+        DBG_PRINTLN(F("IR: COL 3"));
         break;
 
       case 0xF7D02F:        // FIRE
         curMode = 2;
-        DBG_PRINTLN("IR: FIRE");
+        DBG_PRINTLN(F("IR: FIRE"));
         break;
 
       case 0xF730CF:        // COL 4
@@ -357,7 +406,7 @@ void loop()
         rgbData[0] = 255;
         rgbData[1] = 52;
         rgbData[2] = 0;
-        DBG_PRINTLN("IR: COL 4");
+        DBG_PRINTLN(F("IR: COL 4"));
         break;
 
       case 0xF7B04F:        // COL 5
@@ -365,7 +414,7 @@ void loop()
         rgbData[0] = 0;
         rgbData[1] = 255;
         rgbData[2] = 132;
-        DBG_PRINTLN("IR: COL 5");
+        DBG_PRINTLN(F("IR: COL 5"));
         break;
 
       case 0xF7708F:        // COL 6
@@ -373,7 +422,7 @@ void loop()
         rgbData[0] = 58;
         rgbData[1] = 0;
         rgbData[2] = 255;
-        DBG_PRINTLN("IR: COL 6");
+        DBG_PRINTLN(F("IR: COL 6"));
         break;
 
       case 0xF708F7:        // COL 7
@@ -381,7 +430,7 @@ void loop()
         rgbData[0] = 255;
         rgbData[1] = 94;
         rgbData[2] = 0;
-        DBG_PRINTLN("IR: COL 7");
+        DBG_PRINTLN(F("IR: COL 7"));
         break;
 
       case 0xF78877:        // COL 8
@@ -389,7 +438,7 @@ void loop()
         rgbData[0] = 0;
         rgbData[1] = 255;
         rgbData[2] = 193;
-        DBG_PRINTLN("IR: COL 8");
+        DBG_PRINTLN(F("IR: COL 8"));
         break;
 
       case 0xF748B7:        // COL 9
@@ -397,12 +446,12 @@ void loop()
         rgbData[0] = 130;
         rgbData[1] = 0;
         rgbData[2] = 255;
-        DBG_PRINTLN("IR: COL 9");
+        DBG_PRINTLN(F("IR: COL 9"));
         break;
 
       case 0xF7C837:        // RAINBOW
         curMode = 1;
-        DBG_PRINTLN("IR: RAINBOW");
+        DBG_PRINTLN(F("IR: RAINBOW"));
         break;
 
       case 0xF728D7:        // COL 10
@@ -410,7 +459,7 @@ void loop()
         rgbData[0] = 255;
         rgbData[1] = 156;
         rgbData[2] = 0;
-        DBG_PRINTLN("IR: COL 10");
+        DBG_PRINTLN(F("IR: COL 10"));
         break;
 
       case 0xF7A857:        // COL 11
@@ -418,7 +467,7 @@ void loop()
         rgbData[0] = 0;
         rgbData[1] = 255;
         rgbData[2] = 255;
-        DBG_PRINTLN("IR: COL 11");
+        DBG_PRINTLN(F("IR: COL 11"));
         break;
 
       case 0xF76897:        // COL 12
@@ -426,7 +475,7 @@ void loop()
         rgbData[0] = 255;
         rgbData[1] = 0;
         rgbData[2] = 33;
-        DBG_PRINTLN("IR: COL 12");
+        DBG_PRINTLN(F("IR: COL 12"));
         break;
 
 
